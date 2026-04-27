29dcb25c2910f5498c396e897a05d98e2d26d9c4
Brad-87 <brad.tito@gmail.com>
2026-04-27 20:26:21 +1000

    work on image class

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-import cv2, os, time
+import cv2, os, time, sys
 import tkinter as tk
 
+db = 1 # debug info
 
 # Handle loading and storing the image and the modified image.
 class GameImage:
-    pass
+
+    def __init__(self):
+        self.image = None
+        self.modified = None
+        self.height = None
+        self.width = None
+
+    def load_image(self, filename):
+        try:
+            # Build the full file path and read and store the image
+            path = os.path.join(os.path.dirname(__file__), filename)
+            self.image = cv2.imread(path)
+            
+            # Make sure something was read into image variable
+            if self.image is None:
+                print("Error: Failed to load image")
+                sys.exit()
+
+            # Get image metrics, and create an image copy we can modify
+            self.height, self.width, _ = self.image.shape
+            self.modified = self.image.copy()
+
+            if db:
+                print(f"File '{path}' loaded. width:{self.width} height:{self.height}")
+
+        except Exception as errormsg:
+            print("Error:", errormsg)
+
+        
 
 # Represent a hidden difference region, and detects if it's been clicked.
 class Difference:
@@ -16,4 +45,5 @@ class Game:
 
 
 if __name__ == "__main__":
-    print("Hello there")
+    image = GameImage()
+    image.load_image("test.png")
