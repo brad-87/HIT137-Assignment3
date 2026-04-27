3ee542a85913b1b481d0f418d099f073100245e6
Brad-87 <brad.tito@gmail.com>
2026-04-27 19:55:36 +1000

    basic stuff in main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2, os, time
+import tkinter as tk
+
+
+# Handle loading and storing the image and the modified image.
+class GameImage:
+    pass
+
+# Represent a hidden difference region, and detects if it's been clicked.
+class Difference:
+    pass
+
+# Controls game state, user interaction, scoring, etc.
+class Game:
+    pass
+
+
+if __name__ == "__main__":
+    print("Hello there")
